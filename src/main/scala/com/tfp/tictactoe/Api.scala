1e5f95f20d561c039cf126c682ffaaead6960886
Chandan <chandan.ks@outlook.com>
2016-05-21 01:18:08 +0530

    imports optimized

--- src/main/scala/com/tfp/tictactoe/Api.scala
+++ src/main/scala/com/tfp/tictactoe/Api.scala
@@ -1,6 +1,6 @@
 package com.tfp.tictactoe
 
-import com.tfp.tictactoe.TicTacToe.{allWinningCombinations, initialAllEmptyCells}
+import com.tfp.tictactoe.TicTacToe.allWinningCombinations
 
 object Api {
 
